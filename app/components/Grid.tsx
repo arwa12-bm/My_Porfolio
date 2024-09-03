2e6bfa8e711c58cfd070d48570cf0b6ea4732c14
arwa12-bm <arwaarij12344@gmail.com>
2024-09-03 16:15:38 +0100

    final

--- app/components/Grid.tsx
+++ app/components/Grid.tsx
@@ -5,14 +5,19 @@ import { gridItems } from '@/data'
 const Grid = () => {
   return (
 <section id="about">
-    <BentoGrid>
+    <BentoGrid className="w-full py-20">
         {gridItems.map
-        ((item)=>(
+        ((item,i,)=>(
             <BentoGridItem 
             id={item.id}
-            key={item.id}
+            key={i}
             title={item.title}
             description={item.description}
+            className={item.className}
+            img={item.img}
+            imgClassName={item.imgClassName}
+            titleClassName={item.titleClassName}
+            spareImg={item.spareImg}
             />
             ))}
     </BentoGrid>
--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,26 @@ import Image from "next/image";
 import Hero from "./components/Hero";
 import { FaHome } from "react-icons/fa";
 import { FloatingNav } from "./components/ui/FloatingNav";
+import Grid from "./components/Grid";
+import RecentProject from "./components/RecentProject";
+import { navItems } from "@/data";
+import Clients from "./components/Clients";
+import Experience from "./components/Experience";
+import Approach from "./components/Approach";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative overflow-clip bg-black-100 flex justify-center items-center flex-col  mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[
-          {name: "Home", link:'/', icon: <FaHome />}
-          ]} />
+        <FloatingNav navItems={navItems} />
         <Hero />
+        <Grid />
+        <RecentProject />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
       </div>
     </main>
   );
