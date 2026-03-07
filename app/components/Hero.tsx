e5a352ca93b154da7ae75a4b5c59ebd333ed9219
arwa12-bm <arwaarij12344@gmail.com>
2026-03-07 14:25:49 +0100

    fix spline viewer types

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -6,9 +6,10 @@ import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 import Script from "next/script";
 
 const Hero = () => {
+
     return (
         <div className="pb-20 ">
-        
+
             <div>
                 <Spotlight
                     className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
